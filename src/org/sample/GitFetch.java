302385092d050e5e08d4a41f08d0b7b3522b9d72
Prabakaranbalu <prabakaranbalu123@gmail.com>
2019-07-02 09:37:49 +0530

    fetch

---
+++ src/org/sample/GitFetch.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class GitFetch {
+	public void greensOmr(String Order) {
+		System.out.println("Place the order"+ Order);		
+	}
+public void greensOmr(char type) {
+	System.out.println("confirm the type"+type);
+}
+public void greensOmr(long number) {
+	System.out.println("Course we study"+number);
+}
+public static void main(String[] args) {
+	GitFetch m=new GitFetch();
+	m.greensOmr("Courses offered");
+	m.greensOmr(45456l);
+	m.greensOmr('A');
+}
+
+
+}
